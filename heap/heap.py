194bbc4d2b99ae0d386ddc784a79f1e4b9491375
Arkadiusz Kusiak <akusiak23@gmail.com>
2019-02-27 15:42:19 -0600

    heap

--- heap/heap.py
+++ heap/heap.py
@@ -1,21 +1,40 @@
 class Heap:
-  def __init__(self):
-    self.storage = []
+    def __init__(self):
+        self.storage = []
 
-  def insert(self, value):
-    pass
+    def insert(self, value):
+        self.storage.append(value)
+        self._bubble_up(len(self.storage) - 1)
 
-  def delete(self):
-    pass
+    def delete(self):
+        retval = self.storage[0]
+        self.storage[0] = self.storage[len(self.storage) - 1]
+        self.storage.pop()
+        self._sift_down(0)
+        return retval
 
-  def get_max(self):
-    pass
+    def get_max(self):
+        return self.storage[0]
 
-  def get_size(self):
-    pass
+    def get_size(self):
+        return len(self.storage)
 
-  def _bubble_up(self, index):
-    pass
+    def _bubble_up(self, index):
+        while (index - 1) // 2 >= 0:
+            if self.storage[(index - 1) // 2] < self.storage[index]:
+                self.storage[index], self.storage[(
+                    index - 1) // 2] = self.storage[(index - 1) // 2], self.storage[index]
+            index = (index - 1) // 2
 
-  def _sift_down(self, index):
-    pass
+    def _sift_down(self, index):
+        while index * 2 + 1 <= len(self.storage) - 1:
+            if index * 2 + 2 > len(self.storage) - 1:
+                max = index * 2 + 1
+            elif self.storage[index * 2 + 1] > self.storage[index * 2 + 2]:
+                max = index * 2 + 1
+            else:
+                max = index * 2 + 2
+
+            if self.storage[max] > self.storage[index]:
+                self.storage[max], self.storage[index] = self.storage[index], self.storage[max]
+            index = max
